c8256e00e69ed668efcf82170141b7fb8f36099b
pras75299 <singh.prashantking@gmail.com>
2025-01-26 19:41:55 +0530

    update auth

--- auth.py
+++ auth.py
@@ -6,14 +6,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def authenticate():
-    bearer_token = os.getenv("BEARER_TOKEN")
+    # Fetch credentials from .env
     api_key = os.getenv("API_KEY")
     api_secret = os.getenv("API_SECRET")
     access_token = os.getenv("ACCESS_TOKEN")
     access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-    # Authenticate using OAuth 1.0a
-    auth = tweepy.OAuth1UserHandler(bearer_token, api_key, api_secret, access_token, access_token_secret)
+    # Authenticate using OAuth 1.0a for posting tweets
+    auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret)
     api = tweepy.API(auth)
 
     try:
